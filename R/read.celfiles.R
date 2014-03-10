43e2ae7310bd09fd254941c1461a1a069bde9767
Benilton Carvalho <bcarvalh@jhsph.edu>
2014-03-10 18:07:43 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit(s).
    Commit information:
    
        Commit id: 629881f00961d958d816cc1c9a6c66b39667ac26
        Commit message:
        Committing forgotten change
        Committed by Benilton Carvalho <beniltoncarvalho at gmail.com>
        Commit date: 2014-03-10T15:07:33-03:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_13/madman/Rpacks/oligo@87256 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.celfiles.R
+++ R/read.celfiles.R
@@ -98,6 +98,7 @@ read.celfiles <- function( ..., filenames, pkgname, phenoData,
   
   out <- new(theClass)
   slot(out, "assayData") <- assayDataNew(exprs=tmpExprs)
+  overwrite <- FALSE
   if (missing(phenoData)){
       phenoData <- basicPhData1(tmpExprs)
   }else{
@@ -180,6 +181,7 @@ read.celfiles2 <- function(channel1, channel2, pkgname, phenoData,
   out <- new(theClass)
   slot(out, "assayData") <- assayDataNew(channel1=channel1Intensities,
                                          channel2=channel2Intensities)
+  overwrite <- FALSE
   if (missing(phenoData)){
       phenoData <- basicPhData2(channel1Intensities,
                                 channel2Intensities)
